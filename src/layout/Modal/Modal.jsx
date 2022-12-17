d4a3c97e9fb0d2e8acd2f83cbe0056f811ab28e4
Mario Pérez Hurtado <marioph10@gmail.com>
2022-12-17 13:01:00 +0100

    Using portal on modal

--- src/layout/Modal/Modal.jsx
+++ src/layout/Modal/Modal.jsx
@@ -1,12 +1,14 @@
+import { createPortal } from 'react-dom'
+
 export default function Modal({ children, onCancel, onConfirm }) {
-  return (
+  return createPortal(
     <>
       <div
         className="fixed top-0 left-0 z-10 w-screen min-h-screen text-center bg-black bg-opacity-60"
         onClick={onCancel}></div>
-      <div className="fixed z-10 w-11/12 max-w-lg p-4 -translate-x-1/2 border rounded-md shadow-md top-20 left-1/2 bg-zinc-800 md:px-8 md:py-6 border-zinc-600 hover:shadow-xl">
+      <div className="fixed z-10 w-11/12 max-w-lg p-4 -translate-x-1/2 border rounded-md shadow-md top-20 left-1/2 bg-zinc-800 md:px-8 md:py-6 border-zinc-600 hover:shadow-xl text-slate-300">
         {children}
-        <div className="flex gap-2 mt-4 ml-auto md:mt-8 w-fit text-slate-300">
+        <div className="flex gap-2 mt-4 ml-auto md:mt-8 w-fit ">
           <button
             onClick={onCancel}
             className="px-2 py-1 transition border rounded-md border-zinc-600 hover:bg-slate-500 bg-neutral-800">
@@ -19,6 +21,7 @@ export default function Modal({ children, onCancel, onConfirm }) {
           </button>
         </div>
       </div>
-    </>
+    </>,
+    document.getElementById('root')
   )
 }
